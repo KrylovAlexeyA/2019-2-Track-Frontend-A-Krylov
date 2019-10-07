ca6d056c3951feee5b93300e9d60073ccadf9362
Alexey Krylov <kakalex208@gmail.com>
2019-10-07 11:16:47 +0300

    tests added

--- convertBytesToHuman.test.js
+++ convertBytesToHuman.test.js
@@ -16,23 +16,23 @@ test('Возвращает false для отрицательных чисел', 
 
 test('Возвращает false для неправильного типа данных', () => {
   expect(convertBytesToHuman('string')).toBe(false)
+  expect(convertBytesToHuman(null)).toBe(false)
+  expect(convertBytesToHuman(undefined)).toBe(false)
+  expect(convertBytesToHuman(Infinity)).toBe(false)
+  expect(convertBytesToHuman(-Infinity)).toBe(false)
+  expect(convertBytesToHuman({some: 'object'})).toBe(false)
+  expect(convertBytesToHuman(['a', 'r', 'r', 'a', 'y'])).toBe(false)
+  expect(convertBytesToHuman({})).toBe(false)
+  expect(convertBytesToHuman([])).toBe(false)
   // ...
 });
 
 test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(1000 * 1.23)).toBe("1.201 KB")
-  // ...
-});
-
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(1024*1024 * 2.12312)).toBe("2.123 MB")
-  // ...
-});
-
-
-test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(1024 * 1024 * 1024 * 3.123231)).toBe("3.123 GB")
   // ...
 });
+
 
 // другая группа проверок
